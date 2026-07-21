dde085968d1088eaf54f2f5fba8d71ccc3ec0161
Devanshu Kumar <thepixelpoet1@gmail.com>
2026-07-21 08:48:16 +0530

    changes

---
+++ Oceanverse_old/miscellaneous/revString.py
@@ -0,0 +1,14 @@
+string = input("Enter the string: ")
+stringList = list(string)
+i=0
+j=len(string)
+tempList = ['']
+print(tempList)
+while(i<j):
+    tempList[0] = stringList[i]
+    stringList[i] = stringList[j-1]
+    stringList[j-1] = tempList[0]
+    i+=1
+    j-=1
+    # print(tempList)
+print("".join(stringList))
--- Oceanverse_old/module_2/18.py
+++ Oceanverse_old/module_2/18.py
@@ -1,14 +1,20 @@
-n = int(input("Enter the number: ")) 
-# k=0
-# for i in range(n):
-#     k+=i
-#     print(k)
+n = int(input("Enter the number of terms: "))
 
-def function(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return function(n - 1) + function(n - 2)
-print(function(n))
+first = 0
+second = 1
+
+for i in range(n):
+    print(first)
+    next=first+second
+    first=second
+    second=next
+
+
+# def function(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1 
+#     else:
+#         return function(n - 1) + function(n - 2)
+# print(function(n))
